9283fb75586dccf23eabbc80932d6018de1880e2
志勇 郑 <zhengzhiyongzhengzhiyong@hotmail.com>
2020-09-14 17:15:23 +0800

    xg

---
+++ CSharp Multi-Thread/MultiThread/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(PrintNumbers);
+            t.Start();
+            PrintNumbers();
+            Console.ReadLine();
+        }
+
+        static void PrintNumbers()
+        {
+            Console.WriteLine("Starting....");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
