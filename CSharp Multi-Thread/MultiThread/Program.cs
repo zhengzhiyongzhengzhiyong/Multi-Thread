d07da010c232bb0fae00adcb71b3028ee440ce8b
志勇 郑 <zhengzhiyongzhengzhiyong@hotmail.com>
2020-09-15 09:04:23 +0800

    xg

--- CSharp Multi-Thread/MultiThread/Program.cs
+++ CSharp Multi-Thread/MultiThread/Program.cs
@@ -7,7 +7,7 @@ namespace MultiThread
     {
         static void Main(string[] args)
         {
-            Thread t = new Thread(PrintNumbers);
+            Thread t = new Thread(PrintNumbersWithDelay);
             t.Start();
             PrintNumbers();
             Console.ReadLine();
@@ -18,6 +18,16 @@ namespace MultiThread
             Console.WriteLine("Starting....");
             for (int i = 0; i < 10; i++)
             {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void PrintNumbersWithDelay()
+        {
+            Console.WriteLine("Starting....");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
                 Console.WriteLine(i);
             }
         }
